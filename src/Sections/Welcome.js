859ffc4696e0b62e071a3318c2fb64fe10979ab0
lukasssicevs <lukass.sicevs@gmail.com>
2021-10-08 17:59:07 +0300

    test3

--- src/Sections/Welcome.js
+++ src/Sections/Welcome.js
@@ -1,18 +1,10 @@
 import "./Welcome.css";
-import { useTranslation } from "react-i18next";
 
 const Welcome = () => {
-  const { t } = useTranslation();
   return (
     <>
       <div className="mainItem welcomeImg"></div>
-      <div className="mainItem welcomeText">
-        {/* <h1>
-          "The world of reality has its limits; the world of imagination is
-          boundless."
-        </h1>
-        <h5>-Jean-Jacques Rousseau </h5> */}
-      </div>
+      <div className="mainItem welcomeText"></div>
     </>
   );
 };
